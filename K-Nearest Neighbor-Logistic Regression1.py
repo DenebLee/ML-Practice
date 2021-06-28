1f2617707b1b8f3d3d6db3f0f51a669379f58749
lgsgst5613 <lgsgst5613@gmail.com>
2021-06-28 16:28:46 +0900

    확률적 경사 하강법까지

--- K-Nearest Neighbor-Logistic Regression1.py
+++ K-Nearest Neighbor-Logistic Regression1.py
@@ -1,6 +1,132 @@
 #%%
 #데이터 준비하기 
+from numpy.random import logistic
 import pandas as pd 
 fish = pd.read_csv('https://bit.ly/fish_csv_data')
 fish.head()
 # %%
+print(pd.unique(fish['Species']))
+# %%
+#데이터프레임에서 열을 선택하는 방법은 간단 데이터프레임에서 원하는 열을 리스트로 나열하면됨
+fish_input = fish[['Weight', 'Length', 'Diagonal', 'Height', 'Width']].to_numpy()
+print(fish_input[:5])
+#데이터프레임에서 여러 열을 선택하면 새로운 데이터프레임이 반환된다
+#%%
+fish_target = fish['Species'].to_numpy()
+# %%
+#사이킷런의 StandardScaler 클래스를 사용해 훈련 세트와 테스트 세트를 표준화 전처리
+from sklearn.model_selection import train_test_split
+train_input, test_input, train_target, test_target = train_test_split(fish_input, fish_target, random_state = 42)
+from sklearn.preprocessing import StandardScaler
+ss = StandardScaler()
+ss.fit(train_input)
+train_scaled = ss.transform(train_input)
+test_scaled = ss.transform(test_input)
+# %%
+#사이킷런의 KNeightborsClassifier 클래스 객체를 만들고 훈련 세트로 모델을 훈련한 다음 훈련 세트와 테스트 세트의 점수를 확인
+from sklearn.neighbors import KNeighborsClassifier
+kn = KNeighborsClassifier(n_neighbors=3)
+kn.fit(train_scaled, train_target)
+print(kn.score(train_scaled, train_target))
+print(kn.score(test_scaled, test_target))
+# %%
+# 로지스틱 회귀는 선형 방정식을 사용한 분류 알고리즘 
+# 다중 분류는 타깃 클래스가 2개 이상인 분류 문제 
+# 시그모이드 함수는 선형 방정식의 출력을 0~ 1사이의 값으로 압축하여 이진 분류를 위해 사용
+# 소프트맥스 함수는 다중 분류에서 여러 선형 방정식의 출력 결과를 정규화 하여 합이 1이 되도록 만듬
+# %%
+# 타깃 데이터에 2개 이상의 클래스가 포함된 문제를 다중 분류
+print(kn.classes_)
+# %%
+# 테스트 세트에 있는 처음 5개 샘플의 타깃값을 예측
+print(kn.predict(test_scaled[ :5 ]))
+
+# %%
+import numpy as np
+proba = kn.predict_proba(test_scaled[:5])
+print(np.round(proba, decimals=4))
+
+# %%
+distance, indexes = kn.kneighbors(test_scaled[3 : 4])
+print(train_target[indexes])
+# %%
+# 로지스틱 회귀는 이름은 회귀이지만 분류모델 선형회귀와 동일하게 선형 방정식을 학습
+# z = a x (Weight) + b x (Lenght) + c x (Diagonal) + d x (Height) + e x (Width) + f
+
+# a,b,c,d,e는 가중치 혹은 계수 
+import numpy as np
+import matplotlib.pyplot as plt
+z = np.arange(-5, 5, 0.1)
+phi = 1 / (1 + np.exp(-z))
+plt.plot(z , phi)
+plt.xlabel('z')
+plt.ylabel('phi')
+plt.show()
+# %%
+#numpy 배열은 True, False값을 전달하여 행을 선택할수 있다
+# 이를 불리언 인덱싱(boolean indexcing)
+
+char_arr = np.array(['A', 'B', 'C', 'D', 'E'])
+print(char_arr[[True, False, True, False, False]])
+# %%
+bream_smelt_indexes = (train_target =='Bream') | (train_target =='Smelt')
+train_bream_smelt = train_scaled[bream_smelt_indexes]
+target_bream_smelt = train_target[bream_smelt_indexes]
+# %%
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+lr.fit(train_bream_smelt, target_bream_smelt)
+
+# %%
+print(lr.predict_proba(train_bream_smelt[:5]))
+
+# %%
+print(lr.predict(train_bream_smelt[:5]))
+# %%
+print(lr.classes_)
+# %%
+# 로지스틱 회귀가 학습한 계수 확인
+print(lr.coef_, lr.intercept_)
+# 여기서 사용된 로지스틱 회귀 모델이 학습한 방정식은
+# z = -0.404 x (Weight) - 0.576 x (Lenght) - 0.663 x (Diagonal) - 1.013 x (Height) - 0.732 x (Width) - 2.161
+#%%
+# train_bream_smelt의 처음 5개 샘플의 z값 출력
+decisions = lr.decision_function(train_bream_smelt[:5])
+print(decisions)
+# %%
+from scipy.special import expit
+print(expit(decisions))
+# %%
+# LogisticRegression 클래스는 기본적으로 반복적인 알고리즘을 사용
+# 또한 기본적으로 릿지 회귀와 같익 ㅖ수의 제곱을 규제 
+# 이런 규제를 L2규제라고함
+# LogisticRegression 클래스로 다중 분류 모델을 훈련하는 코드
+lr = LogisticRegression(C=20, max_iter= 1000)
+lr.fit(train_scaled, train_target)
+print(lr.score(train_scaled, train_target))
+print(lr.score(test_scaled, test_target))
+# %%
+# 테스트 세트의 처음 5개 샘플에 대한 예측 출력
+print(lr.predict(test_scaled[:5]))
+
+# %%
+# 테스트 세트의 처음 5개 샘플에 대한 예측 확률 출력
+proba = lr.predict_proba(test_scaled[:5])
+print(np.round(proba, decimals=3))
+# %%
+print(lr.classes_)
+# %%
+print(lr.coef_.shape, lr.intercept_.shape)
+# 다중 분류는 클래스마다 z값을 하나씩 계산
+# %%
+decision = lr.decision_function(test_scaled[:5])
+print(np.round(decision, decimals=2))
+# %%
+from scipy.special import softmax
+proba = softmax(decision, axis=1)
+print(np.round(proba, decimals=3))
+
+# %%
+# k-최근접 이웃 모델이 확률을 출력할 수 있지만 이웃한 샘플의 클래스 비율이므로 항상 정해진 확률만 출력 
+# 로지스틱 회귀는 이진 분류에서는 하나의 선형 방정식을 훈련
+# 다중 분류일 경우에는 클래스 개수만큼 방정식을 훈련
---
+++ Stochastic Gradient Descent.py
@@ -0,0 +1,89 @@
+# 훈련 세트에서 샘플 하나씩 꺼내 손실 함수의 경사를 따라 최적의 모델을 찾는 알고리즘 
+# 샘플을 하나씩 사용하지 않고 여러 개를 사용하면 미니배치 경사 하강법이 된다 
+# 한번에 전체 샘플을 사용하면 배치 경사 하강법
+
+# 점진적 학습 : 훈련된 모델을 버리지않고 새로운 데이터에 대해서만 조금씩 훈련시키는 방법 
+# 대표적인 점진적 학습 알고리즘 = 확률적 경사 하강법
+
+# 확률적 경사 하강법엣 훈련 세트를 한번에 모두 사용하는 과정을 에포크(eporch)
+#여러개의 샘플을 선택할 경우 미니배치 경사하강법이라고한다 
+# 전체 샘플을 사용할경우 배치 경사 하강법 -> 가장 안정적이지만 자원소모가 심함
+# 확률적 경사 하강법을 꼭 사용하는 알고리즘 = 신경망 알고리즘
+
+#손실함수란 어떤 문제에서 머신러닝 알고리즘이 얼마나 엉터리인지를 측정하는 기준
+# 대부분의 문제에 잘 맞는 손실 함수가 이미 정의되어 있다 . 이진분류에는 로지스틱 회귀( 또는 이진 크로스엔트로피) 손실 함수 사용
+# 다중 분류에는 크로스엔트로피 손실 함수 사용 
+# 회귀 문제에는 평균제곱 오차 손실 함수 사용 
+#%%
+import pandas as pd
+fish = pd.read_csv('https://bit.ly/fish_csv_data')
+# %%
+fish_input = fish[['Weight', 'Length','Diagonal', 'Height', 'Width' ]].to_numpy()
+fish_target = fish['Species'].to_numpy()
+# %%
+#사이킷런의 train_test_split() 함수를 사용해 데이터를 훈련, 테스트 세트로 나눔
+from sklearn.model_selection import train_test_split
+train_input, test_input, train_target, test_target = train_test_split(fish_input, fish_target, random_state = 42)
+# %%
+# 훈련세트와 테스트 세트의 특성을 표준화 전처리 *꼭 훈련세트에서 학습한 통계 값으로 테스트세트도 변환해야함
+from sklearn.preprocessing import StandardScaler
+ss = StandardScaler()
+ss.fit(train_input)
+train_scaled = ss.transform(train_input)
+test_scaled = ss.transform(test_input)
+# %%
+# SGDClassifier의 객체를 만들때 2개의 매개변수 지정
+# loss는 손실 함수의 종류를 지정
+# loss= 'log'로 지정하여 로지스틱 손실 함수 지정 
+# max_iter는 수행할 에포크 횟수를 지정
+from sklearn.linear_model import SGDClassifier
+sc = SGDClassifier(loss='log', max_iter=10, random_state=42)
+sc.fit(train_scaled, train_target)
+# %%
+# 훈련세트와 테스트 세트의 정확도 점수 출력
+print(sc.score(train_scaled, train_target))
+print(sc.score(test_scaled, test_target))
+# 출력된 훈련과 테스트 세트의 정확도가 낮음 횟수 10번은 부족
+# %%
+sc.partial_fit(train_scaled, train_target)
+print(sc.score(train_scaled, train_target))
+print(sc.score(test_scaled, test_target))
+#에포크를 한번더 실행하니 정확도가 향상
+# %%
+# 훈련세트 점수는 에포크가 진행될수록 꾸준히 증가하지만 테스트 세트 점수는 어느 순간 감소하기 시작
+# 바로 이 지점이 해당 모델의 과대적합되기 시작하는곳
+# 과대적합이 시작하기 전에 훈련을 멈추는 것을 조기 종료
+import numpy as np
+sc = SGDClassifier(loss='log', random_state=42)
+train_score = []
+test_score = []
+classes = np.unique(train_target)
+# 300번의 에포크 동안 훈련을 반복하여 진행 
+# 반복마다 훈련세트와 테스트 세트의 점수 계산하여 train과 test score 리스트에 추가
+#%%
+for _ in range(0, 300):
+    sc.partial_fit(train_scaled, train_target, classes = classes)
+    train_score.append(sc.score(train_scaled, train_target))
+    test_score.append(sc.score(test_scaled, test_target))
+# %%
+import matplotlib.pyplot as plt
+plt.plot(train_score)
+plt.plot(test_score)
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.show()
+# 파란색( 훈련세트 그래프) 주황색(테스트 세트 그래프)
+# %%
+sc = SGDClassifier(loss='log', max_iter=100, tol=None, random_state=42)
+sc.fit(train_scaled, train_target)
+print(sc.score(train_scaled, train_target))
+print(sc.score(test_scaled,test_target))
+# SGDClassfier는 일정 에포크 동안 성능이 향상되지 않으면 더 훈련하지 않고 자동으로 멈춘다
+# tol매개변수에서 향상될 최솟값을 지정
+# %%
+# SGDClassifier의 loss 매개변수의 기본값은 'hinge'이다. 힌지 손실은 서포트 벡터 머신이라 불리는 또 다른 머신러닝 알고리즘 을 위한 손실함수
+sc = SGDClassifier(loss='hinge', max_iter=100, tol=None, random_state=42)
+sc.fit(train_scaled, train_target)
+print(sc.score(train_scaled, train_target))
+print(sc.score(test_scaled, test_target))
+# %%
